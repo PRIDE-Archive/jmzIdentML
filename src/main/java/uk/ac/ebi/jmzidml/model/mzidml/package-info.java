81c4763992b9d3610eb198a958023021271f3a60
selvakumar kamatchinathan <selva.439@gmail.com>
2022-08-05 19:49:09 +0100

    1.2Mzidschema

--- src/main/java/uk/ac/ebi/jmzidml/model/mzidml/package-info.java
+++ src/main/java/uk/ac/ebi/jmzidml/model/mzidml/package-info.java
@@ -1,2 +1,2 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://psidev.info/psi/pi/mzIdentML/1.1", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://psidev.info/psi/pi/mzIdentML/1.2", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
 package uk.ac.ebi.jmzidml.model.mzidml;
